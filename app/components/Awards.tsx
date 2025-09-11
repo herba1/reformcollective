afc4b1563ab2a7af9edf6e1e973befa448373eb3
herbart hernandez <burnedcard77@gmail.com>
2025-09-10 18:19:35 -0700

    awards wip2

--- app/components/Awards.tsx
+++ app/components/Awards.tsx
@@ -1,37 +1,32 @@
-
-
 import { Instrument_Serif } from "next/font/google";
 import Image from "next/image";
 
 const instrumentSerif = Instrument_Serif({
-    subsets: ["latin"],
-    weight: ["400"],
+  subsets: ["latin"],
+  weight: ["400"],
 });
 
-export default function Awards(){
-
-    return(
-        <section className=" h-[150lvh] outline-1 outline-black/30  ">
-            <article className="mx-5">
-                <h2 className={` flex flex-col text-6xl leading-none `}>
-                    <span className={` ${instrumentSerif.className}`}>Awards</span>
-                    <span className=" tracking-tighter">& Recognitions</span>
-                </h2>
-                <div>
-                    <div>
-                        <Image height={100} width={100} alt="runway logo" src={'/runway.svg'}></Image>
-                    <p>
-
-                    </p>
-
-                    </div>
-                    <ul></ul>
-
-                </div>
-
-            </article>
-
-        </section>
-    )
-
+export default function Awards() {
+  return (
+    <section className="h-[150lvh] outline-1 outline-black/30">
+      <article className="mx-5">
+        <h2 className={`flex flex-col text-6xl leading-none`}>
+          <span className={` ${instrumentSerif.className}`}>Awards</span>
+          <span className="tracking-tighter">& Recognitions</span>
+        </h2>
+        <div>
+          <div>
+            <Image
+              height={100}
+              width={100}
+              alt="runway logo"
+              src={"/runway.svg"}
+            ></Image>
+            <p></p>
+          </div>
+          <ul></ul>
+        </div>
+      </article>
+    </section>
+  );
 }
